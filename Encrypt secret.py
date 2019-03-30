da4fc3cd4db884464b771b1095d6402df6ea11df
r.dubatov <roman.dubatov@gmail.com>
2019-03-30 13:43:21 +0300

    It's work!

---
+++ Encrypt secret.py
@@ -0,0 +1,47 @@
+import hashlib
+# трафик между NAS и сервером
+# Accounting-Response
+Code = 0x05
+ID = 0x2e
+Length = 0x00
+leng2 = 0x14
+# Accounting-Request
+Auth = [0x8e, 0xad, 0x44, 0x0e, 0xcc, 0x45, 0x1a, 0xf7, 0x94, 0xcb, 0xc6, 0x41, 0x51, 0x78, 0x88, 0x17]
+secret = []  # DEAF99
+mess_for_hash = []
+Chall_Auth = "f91afe56c6f36e69893cf4c41baae06e" # Accounting-Response
+
+count = 0
+
+mess_for_hash.append(Code)
+mess_for_hash.append(ID)
+mess_for_hash.append(Length)
+mess_for_hash.append(leng2)
+mess_for_hash += Auth
+
+def GetHash(str):
+    hash = hashlib.md5(bytes(str))
+    dec_mess = hash.hexdigest()
+    return dec_mess
+
+def StrInHex(str):
+    auth = ""
+    for i in range(0, len(str), 2):
+        auth += "0x" + str[i:i+2] + ", "
+    print(auth)
+
+def GenNum(list, i):
+    for n in range(48, 71):
+        list[i] = n
+        if(i > 0):
+            GenNum(list, i - 1)
+        mess = GetHash(mess_for_hash + list)
+
+        if (mess == Chall_Auth):
+            print(list)
+
+def GetSecret():
+    for i in range(6):                          # длина пароля
+        GenNum([0 for i in range(i+1)], i)
+
+GetSecret()
---
+++ Encrypt_password.py
@@ -0,0 +1,41 @@
+import hashlib
+
+# трафик между клиентом и NAS server
+secret = "DEAF99"
+secret_dec = [68, 69, 65, 70, 57, 57]
+ID = 0x02   # Request
+password = 0  # 42CA11
+#Request
+Auth = [0xff, 0x97, 0x9f, 0xae, 0x4f, 0xba, 0x32, 0x74, 0x60, 0x5b, 0x88, 0x7c, 0x4e, 0x92, 0xc1, 0x01]
+Send_Val = "8804a297678e813c88fca0cd3dd499e3" #Responce
+
+def StrInHex(str):
+    auth = ""
+    for i in range(0, len(str), 2):
+        auth += "0x" + str[i:i+2] + ", "
+    print(auth)
+
+def GetHash(str):
+    hash = hashlib.md5(bytes(str))
+    dec_mess = hash.hexdigest()
+    return dec_mess
+
+def GenNum(list, i):
+    for n in range(48, 71):
+        list[i] = n
+        if(i > 0):
+            GenNum(list, i - 1)
+        mess = []
+        mess.append(ID)
+        mess += list + Auth
+        decr = GetHash(mess)
+
+        if (decr == Send_Val):
+            print(list)
+
+def GetPassword():
+    for i in range(6):                          # длина пароля
+        GenNum([0 for i in range(i+1)], i)
+
+
+GetPassword()
